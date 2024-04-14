aa8602cf4318312fa3f6e6dc8fb536af73d7b568
Tom Bischopink <s87622@informatik.htw-dresden.de>
2024-04-14 14:37:33 +0200

    update

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
 from flask import Flask, render_template, request, jsonify
-
+import numpy as np
+from src.models import run_modell
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/submit', methods=['POST'])
 def handle_data():
     data = request.get_json()
-    print("Received data:", data)
-    return jsonify({"status": "Data received"}), 200
+    total_profit, total_revenue, mean_revenue, mean_profit, mean_temperaturen, days = run_modell(
+        int(data["anzahl"]))
+    
+    mean_revenue = mean_revenue.tolist()
+    mean_profit = mean_profit.tolist()
+    mean_temperaturen = mean_temperaturen.tolist()
+    days = days.tolist()
+    return jsonify({"total_profit": total_profit, 
+                    "total_revenue": total_revenue,
+                    "mean_revenue": mean_revenue,
+                    "mean_profit": mean_profit,
+                    "mean_temperature": mean_temperaturen,
+                    "days": days}), 200
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/index.js
+++ static/index.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const button = document.querySelector('.button'); // Ensuring to select your button correctly, add an ID if necessary.
+    const button = document.querySelector('.button'); // Make sure the button selector is correct
     button.addEventListener('click', function(e) {
         e.preventDefault();
+
+        // Collect input data
         const anzahl = document.getElementById('anzahlInput').value;
         const fixkosten = document.getElementById('fixkostenInput').value;
         const variableKosten = document.getElementById('variableKostenInput').value;
         const temperatur = document.getElementById('temperaturInput').value;
 
+        // Make POST request to the server
         fetch('/submit', {
             method: 'POST',
             headers: {
@@ -15,7 +18,11 @@ document.addEventListener('DOMContentLoaded', function() {
             body: JSON.stringify({ anzahl, fixkosten, variableKosten, temperatur })
         })
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+            // Display total_profit in the output field
+            document.getElementById('outputProfit').value = data.total_profit;
+            document.getElementById('outputUmsatz').value = data.total_revenue;
+        })
         .catch(error => console.error('Error:', error));
     });
 });
